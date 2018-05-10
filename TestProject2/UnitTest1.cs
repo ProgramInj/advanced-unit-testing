48efc755a4f43bb46ed13efebd4036e1734a2a00
Adima <777adima@bk.ru>
2018-05-11 00:00:43 +0400

    Tests done

---
+++ TestProject2/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using МодТестЧасть2;
+
+namespace TestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()//testFront
+        {
+            DEQueue<string> qwsda = new DEQueue<string>();
+            qwsda.pushBack("KAte");
+            qwsda.pushBack("LOLQOP");
+            Assert.AreEqual(qwsda.front(), "KAte");
+        }
+
+        [TestMethod]
+        public void TestMethod2()//test_popfront
+        {
+            DEQueue<string> qwsda = new DEQueue<string>();
+            qwsda.pushBack("a");
+            qwsda.pushBack("b");
+            qwsda.pushBack("c");
+            qwsda.pushBack("d");
+            string a = qwsda.popFront() + qwsda.popFront() + qwsda.popFront() + qwsda.popFront();
+            Assert.AreEqual(a, "abcd");
+        }
+        [TestMethod]
+        public void TestMethod3()//test_popback
+        {
+            DEQueue<string> qwsda = new DEQueue<string>();
+            qwsda.pushBack("a");
+            qwsda.pushBack("b");
+            qwsda.pushBack("c");
+            qwsda.pushBack("d");
+            string a = qwsda.popBack() + qwsda.popBack() + qwsda.popBack() + qwsda.popBack();
+            Assert.AreEqual(a, "dcba");
+        }
+
+        [TestMethod]
+        public void TestMethod4()//test_popfront_popback
+        {
+            DEQueue<string> qwsda = new DEQueue<string>();
+            qwsda.pushBack("a");
+            qwsda.pushBack("b");
+            qwsda.pushBack("c");
+            qwsda.pushBack("d");
+            string a = qwsda.popFront() + qwsda.popBack() + qwsda.popFront() + qwsda.popBack();
+            Assert.AreEqual(a, "adbc");
+        }
+
+        [TestMethod]
+        public void TestMethod5()//test_toArray
+        {
+            DEQueue<int> qwsda = new DEQueue<int>();
+            qwsda.pushBack(1);
+            qwsda.pushBack(2);
+            qwsda.pushBack(3);
+            qwsda.pushBack(4);
+            int[] a = new int[4];
+            a[0] = 1; a[1] = 2; a[2] = 3; a[3] = 4;
+            int[] b = qwsda.toArray();
+            CollectionAssert.AreEqual(a, b);
+        }
+
+    }
+}
---
+++ МодТестЧасть2/DEQueue .cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace МодТестЧасть2
+{
+    public class Num<str>
+    {
+        public Num(str data)
+        {
+            Data = data;
+        }
+        public str Data { get; set; }
+        public Num<str> Next { get; set; }
+        public Num<str> Prev { get; set; }
+    }
+
+    public class DEQueue<str>
+    {
+
+        Num<str> first;
+        Num<str> last;
+        int count;
+
+        public void pushBack(str data)
+        {
+            Num<str> node = new Num<str>(data);
+            Num<str> temp = last;
+            last = node;
+            if (count == 0)
+                first = last;
+            else
+            {
+                temp.Next = last;
+                last.Prev = temp;
+            }
+            count++;
+        }
+
+        public void pushFront(str data)
+        {
+            Num<str> node = new Num<str>(data);
+            Num<str> temp = first;
+            first = node;
+            if (count == 0)
+                last = first;
+            else
+            {
+                temp.Prev = first;
+                first.Next = temp;
+            }
+            count++;
+        }
+
+        public str front()
+        {
+            if (count != 0)
+            {
+                return first.Data;
+            }
+            else
+                throw new InvalidOperationException();
+        }
+        public str back()
+        {
+            if (count != 0)
+            {
+                return last.Data;
+            }
+            else
+                throw new InvalidOperationException();
+        }
+        public int size()
+        {
+            return count;
+        }
+        public void clear()
+        {
+            count = 0;
+        }
+        public str popFront()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            str output = first.Data;
+            first = first.Next;
+            count--;
+            return output;
+        }
+        public str popBack()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            str output = last.Data;
+            last = last.Prev;
+            count--;
+            return output;
+        }
+
+
+        public str[] toArray()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            str[] output = new str[count];
+            Num<str> temp = first;
+            for (int i = 0; i < count; i++)
+            {
+                output[i] = temp.Data;
+                temp = temp.Next;
+            }
+            return output;
+        }
+
+
+
+    }
+}
